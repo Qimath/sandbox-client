7cf4a2ed6785e5ebe325efc00e62b6097bf9f12e
Qimath <vibert.christopher@gmail.com>
2023-07-11 17:52:56 +0200

    function

---
+++ dist/assets/AccountView-83f417f9.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index-ca413c20.js";const t={};function e(n,r){return"Account"}const _=c(t,[["render",e]]);export{_ as default};
--- dist/assets/AccountView-fb288d5f.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./index-839c82e3.js";const t={};function e(n,r){return"Account"}const _=c(t,[["render",e]]);export{_ as default};
