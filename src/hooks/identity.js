319b77021047ac202f873e56628e1e1440d43a41
Qimath <vibert.christopher@gmail.com>
2024-08-19 00:42:21 +0200

    gotrue session duration update

--- src/hooks/identity.js
+++ src/hooks/identity.js
@@ -166,7 +166,7 @@ export async function authCallback(accessToken) {
     token: {
       access_token: accessToken,
       token_type: "bearer",
-      expires_in: 3600,
+      expires_in: 2592000,
       expires_at: userData.exp * 1000,
     },
     id: userData.sub,
