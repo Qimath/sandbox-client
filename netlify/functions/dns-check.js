4a94e71e054ee3386cde5b373a9a1c2b83fbad91
Qimath <vibert.christopher@gmail.com>
2024-08-19 19:50:50 +0200

    dns checker improvements

--- netlify/functions/dns-check.js
+++ netlify/functions/dns-check.js
@@ -12,184 +12,228 @@ class DnsChecker {
 
   async resolve() {
     try {
-      await Promise.all(
-        this.requiredRecords.map((record) => this.resolveRecord(record))
+      const recordPromises = this.requiredRecords.map((record) =>
+        this.processRecord(record)
       );
+      await Promise.all(recordPromises);
     } catch (error) {
       console.error("Error resolving DNS records:", error);
     }
-    this.compareRecords();
+
+    this.evaluateResults();
     this.sortResults();
-    return {
-      success: !this.hasIssues,
-      data: this.results,
-    };
+    return this.generateResponse();
   }
 
-  async resolveRecord(record) {
-    const targetsToCheck = [];
-
-    targetsToCheck.push(
-      record.target ? `${record.target}.${this.domain}` : this.domain
-    );
-
-    if (record.name === "dmarc-cname" || record.name === "spf") {
-      const parts = this.domain.split(".");
-      if (parts.length > 2) {
-        const organizationalDomain = parts.slice(-2).join(".");
-        if (record.target) {
-          targetsToCheck.push(`${record.target}.${organizationalDomain}`);
-        } else {
-          targetsToCheck.push(organizationalDomain);
-        }
-      }
-    }
-
-    let addresses = [];
-    for (const targetDomain of targetsToCheck) {
-      addresses = await this.resolveDns(targetDomain, record.type);
-      if (
-        addresses &&
-        addresses.length > 0 &&
-        addresses[0] !== "No record found"
-      ) {
-        this.addRecord(record.name, targetDomain, record.type, addresses);
+  async processRecord(record) {
+    const targets = this.getTargetsToCheck(record);
+    for (const target of targets) {
+      let addresses = await this.resolveDns(target, record.type);
+      if (this.isValidRecord(addresses)) {
+        this.storeRecord(record.name, target, record.type, addresses);
         return;
       }
 
+      // DMARC fallback: Check TXT record if CNAME is not found
       if (
         record.name === "dmarc-cname" &&
-        addresses.includes("No record found")
+        this.isDmarcFallbackNeeded(addresses)
       ) {
-        addresses = await this.resolveDns(targetDomain, "TXT");
-        if (
-          addresses &&
-          addresses.length > 0 &&
-          addresses[0] !== "No record found"
-        ) {
-          record.type = "TXT";
-          this.addRecord(record.name, targetDomain, "TXT", addresses);
+        addresses = await this.resolveDns(target, "TXT");
+        if (this.isValidRecord(addresses) && this.isDmarcTxtValid(addresses)) {
+          this.storeRecord(record.name, target, "TXT", addresses);
           return;
         }
       }
     }
-
-    this.addRecord(record.name, targetsToCheck[0], record.type, [
-      "No record found",
-    ]);
+    this.storeRecord(record.name, targets[0], record.type, ["No record found"]);
   }
 
-  resolveDns(domain, type) {
-    return dns.resolve(domain, type).catch((err) => {
-      if (err.code === "ENOTFOUND" || err.code === "ENODATA") {
-        return ["No record found"];
-      }
-      throw err;
-    });
+  getTargetsToCheck(record) {
+    const baseTarget = record.target
+      ? `${record.target}.${this.domain}`
+      : this.domain;
+    const targets = [baseTarget];
+
+    if (record.name === "dmarc-cname") {
+      const orgDomain = this.getOrganizationalDomain();
+      if (orgDomain)
+        targets.push(
+          record.target ? `${record.target}.${orgDomain}` : orgDomain
+        );
+    }
+
+    if (record.name === "caa") {
+      const subDomains = this.getSubDomains();
+      targets.push(...subDomains);
+    }
+
+    return targets;
   }
 
-  addRecord(name, location, type, values) {
+  getOrganizationalDomain() {
+    const parts = this.domain.split(".");
+    return parts.length > 2 ? parts.slice(-2).join(".") : null;
+  }
+
+  getSubDomains() {
+    const parts = this.domain.split(".");
+    return parts.map((_, i) => parts.slice(i).join(".")).slice(1);
+  }
+
+  async resolveDns(domain, type) {
+    try {
+      return await dns.resolve(domain, type);
+    } catch (err) {
+      return err.code === "ENOTFOUND" || err.code === "ENODATA"
+        ? ["No record found"]
+        : Promise.reject(err);
+    }
+  }
+
+  isValidRecord(addresses) {
+    return (
+      addresses && addresses.length > 0 && addresses[0] !== "No record found"
+    );
+  }
+
+  isDmarcFallbackNeeded(addresses) {
+    return addresses.includes("No record found");
+  }
+
+  isDmarcTxtValid(addresses) {
+    return addresses.some((address) => /^v=DMARC1/.test(address));
+  }
+
+  storeRecord(name, location, type, values) {
     if (!this.retrievedRecords[name]) {
       this.retrievedRecords[name] = [];
     }
-
-    const formattedValues = values.map((value) => {
-      if (Array.isArray(value)) return value.join(" ");
-      if (typeof value === "object" && type === "MX")
-        return `${value.priority} ${value.exchange}`;
-      return value;
-    });
-
+    const formattedValues = this.formatRecordValues(type, values);
     formattedValues.forEach((value) => {
       if (location) {
         this.retrievedRecords[name].push({ location, type, value });
       } else {
         console.error(
-          `Attempted to add a record with an undefined location (target): ${name}, ${type}, ${value}`
+          `Undefined location for record: ${name}, ${type}, ${value}`
         );
       }
     });
   }
 
-  compareRecords() {
+  formatRecordValues(type, values) {
+    return values.map((value) => {
+      if (Array.isArray(value)) return value.join(" ");
+      if (typeof value === "object") return this.formatObjectValue(type, value);
+      return value;
+    });
+  }
+
+  formatObjectValue(type, value) {
+    if (type === "MX") return `${value.priority} ${value.exchange}`;
+    if (type === "CAA") return this.formatCaaValue(value);
+    return value;
+  }
+
+  formatCaaValue(value) {
+    const critical =
+      value.critical !== undefined ? value.critical : "undefined";
+    const tag = value.issue
+      ? "issue"
+      : value.issuewild
+      ? "issuewild"
+      : "undefined";
+    const recordValue = value.issue || value.issuewild || "undefined";
+    return `${critical} ${tag} "${recordValue}"`;
+  }
+
+  evaluateResults() {
     this.requiredRecords.forEach((record) => {
       const retrieved = this.retrievedRecords[record.name] || [];
-      let match = retrieved.find((r) =>
-        record.type === "MX"
-          ? record.regex.test(r.value.split(" ")[1])
-          : record.regex.test(r.value)
-      );
-
-      if (!match && record.name === "dmarc-cname") {
-        match = retrieved.find(
-          (r) => r.type === "TXT" && /^v=DMARC1/.test(r.value)
-        );
-        if (match) {
-          this.results.push({
-            found: true,
-            expected: {
-              type: "CNAME",
-              target: record.target
-                ? `${record.target}.${this.domain}`
-                : this.domain,
-              value: record.expect,
-            },
-            retrieved: [
-              {
-                type: "TXT",
-                target: match.location,
-                value: match.value,
-              },
-            ],
-          });
-          return;
-        }
-      }
-
-      if (!match) {
-        this.results.push({
-          found: false,
-          expected: {
-            type: record.type,
-            target: record.target
-              ? `${record.target}.${this.domain}`
-              : this.domain,
-            value: record.expect,
-          },
-          retrieved: retrieved.map((r) => ({
-            type: r.type,
-            target: r.location || "undefined target",
-            value: r.value,
-          })),
-        });
-        this.hasIssues = true;
-      } else {
-        this.results.push({
-          found: true,
-          expected: {
-            type: record.type,
-            target: record.target
-              ? `${record.target}.${this.domain}`
-              : this.domain,
-            value: record.expect,
-          },
-          retrieved: retrieved.map((r) => ({
-            type: r.type,
-            target: r.location || "undefined target",
-            value: r.value,
-          })),
-        });
-      }
+      const match = this.evaluateRecord(record, retrieved);
+      this.results.push(match);
+      if (!match.found) this.hasIssues = true;
     });
+  }
+
+  evaluateRecord(record, retrieved) {
+    if (record.type === "MX") {
+      return this.evaluateMxRecord(record, retrieved);
+    } else if (record.name === "caa") {
+      return this.evaluateCaaRecord(record, retrieved);
+    } else {
+      return this.evaluateGenericRecord(record, retrieved);
+    }
+  }
+
+  evaluateMxRecord(record, retrieved) {
+    const expectedMatch = retrieved.find((r) =>
+      record.regex.test(r.value.split(" ")[1])
+    );
+    if (!expectedMatch) return this.createResult(false, record, retrieved);
+
+    const expectedPriority = parseInt(expectedMatch.value.split(" ")[0], 10);
+    const higherPriorityExists = retrieved.some((r) => {
+      const priority = parseInt(r.value.split(" ")[0], 10);
+      return (
+        priority < expectedPriority && !record.regex.test(r.value.split(" ")[1])
+      );
+    });
+
+    return this.createResult(!higherPriorityExists, record, retrieved);
+  }
+
+  evaluateCaaRecord(record, retrieved) {
+    const unrestricted =
+      retrieved.length === 0 ||
+      retrieved.every((r) => r.value === "No record found");
+    const match =
+      unrestricted || retrieved.some((r) => record.regex.test(r.value));
+    return this.createResult(match, record, retrieved);
+  }
+
+  evaluateGenericRecord(record, retrieved) {
+    let match = retrieved.find((r) => record.regex.test(r.value));
+    if (!match && record.name === "dmarc-cname") {
+      match = retrieved.find(
+        (r) => r.type === "TXT" && /^v=DMARC1/.test(r.value)
+      );
+    }
+    return this.createResult(!!match, record, retrieved);
+  }
+
+  createResult(found, record, retrieved) {
+    return {
+      found,
+      expected: {
+        type: record.type,
+        target: record.target ? `${record.target}.${this.domain}` : this.domain,
+        value: record.expect,
+      },
+      retrieved: retrieved.map((r) => ({
+        type: r.type,
+        target: r.location || "undefined target",
+        value: r.value,
+      })),
+    };
   }
 
   sortResults() {
-    const order = ["TXT", "CNAME", "MX"];
+    const order = ["TXT", "CNAME", "MX", "CAA"];
     this.results.sort(
       (a, b) => order.indexOf(a.expected.type) - order.indexOf(b.expected.type)
     );
   }
+
+  generateResponse() {
+    return {
+      success: !this.hasIssues,
+      data: this.results,
+    };
+  }
 }
+
+// Subclasses for each DNS type
 
 class StatusDns extends DnsChecker {
   constructor(domain, websiteId) {
@@ -207,6 +251,13 @@ class StatusDns extends DnsChecker {
         target: "",
         expect: "custom.crisp.watch.",
         regex: new RegExp(`^custom\\.crisp\\.watch\\.?$`),
+      },
+      {
+        name: "caa",
+        type: "CAA",
+        target: "",
+        expect: 'issue "letsencrypt.org"',
+        regex: new RegExp('issue "letsencrypt.org"'),
       },
     ]);
   }
@@ -228,6 +279,13 @@ class HelpdeskDns extends DnsChecker {
         target: "",
         expect: "custom.crisp.help.",
         regex: new RegExp(`^custom\\.crisp\\.help\\.?$`),
+      },
+      {
+        name: "caa",
+        type: "CAA",
+        target: "",
+        expect: 'issue "letsencrypt.org"',
+        regex: new RegExp('issue "letsencrypt.org"'),
       },
     ]);
   }
@@ -278,6 +336,13 @@ class EmailDns extends DnsChecker {
         expect: "links.custom.crisp.email.",
         regex: new RegExp(`^links\\.custom\\.crisp\\.email\\.?$`),
       },
+      {
+        name: "caa",
+        type: "CAA",
+        target: "",
+        expect: 'issue "letsencrypt.org"',
+        regex: new RegExp('issue "letsencrypt.org"'),
+      },
     ]);
   }
 }
@@ -295,21 +360,24 @@ export async function handler(event, context) {
       };
     }
 
-    const dnsClasses = {
-      status: StatusDns,
-      helpdesk: HelpdeskDns,
-      email: EmailDns,
-    };
-    const DnsClass = dnsClasses[type];
-
-    if (!DnsClass) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: `Invalid type: ${type}` }),
-      };
+    let dnsChecker;
+    switch (type) {
+      case "status":
+        dnsChecker = new StatusDns(domain, websiteId);
+        break;
+      case "helpdesk":
+        dnsChecker = new HelpdeskDns(domain, websiteId);
+        break;
+      case "email":
+        dnsChecker = new EmailDns(domain, websiteId);
+        break;
+      default:
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: `Invalid type: ${type}` }),
+        };
     }
 
-    const dnsChecker = new DnsClass(domain, websiteId);
     const results = await dnsChecker.resolve();
     return {
       statusCode: 200,
